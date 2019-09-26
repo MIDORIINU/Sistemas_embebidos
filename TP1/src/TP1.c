5fb18c9343bab60b227f71c3147c17038f4bf860
MIDORIINU <diegorluna@gmail.com>
2019-09-26 03:10:24 -0300

    Correcciones.

--- TP1/src/TP1.c
+++ TP1/src/TP1.c
@@ -1,5 +1,10 @@
 /*
         TP1
+
+        Diego Luna
+        Daniel Romero
+        Juan C. Suárez
+
 */
 
 
@@ -9,18 +14,6 @@
 #include "sapi.h"       // <= sAPI header
 
 /*==================[macros and definitions]=================================*/
-
-/*==================[internal data declaration]==============================*/
-
-/*==================[internal functions declaration]=========================*/
-
-/*==================[internal data definition]===============================*/
-
-/*==================[external data definition]===============================*/
-
-/*==================[internal functions definition]==========================*/
-
-
 #define TP1_1 1
 #define TP1_2 2
 #define TP1_3 3
@@ -28,31 +21,25 @@
 
 #define TP1_PROJECT TP1_3
 
-
-/* The DEBUG* functions are sAPI debug print functions.
-Code that uses the DEBUG* functions will have their I/O routed to
-the sAPI DEBUG UART. */
+/*      The DEBUG* functions are sAPI debug print functions.
+        Code that uses the DEBUG* functions will have their I/O routed to
+        the sAPI DEBUG UART. */
 DEBUG_PRINT_ENABLE;
 
 
+/*==================[internal data declaration]==============================*/
 
+/*==================[internal functions declaration]=========================*/
 #if (TP1_PROJECT == TP1_3)
 /* FUNCION que se ejecuta cada vez que ocurre un Tick. */
-void myTickHook( void *ptr ){
-
-   static bool_t ledState = OFF;
-
-   gpioMap_t led = (gpioMap_t)ptr;
-
-   if( ledState ){
-      ledState = OFF;
-   }
-   else{
-      ledState = ON;
-   }
-   gpioWrite( led, ledState );
-}
+static void myTickHook(void* ptr);
 #endif
+
+
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
 
 
 /*==================[external functions definition]==========================*/
@@ -63,39 +50,29 @@ int main(void)
         /* ------------- INICIALIZACIONES ------------- */
         /* Inicializar la placa */
         boardConfig();
-
         /* UART for debug messages. */
-        debugPrintConfigUart( UART_USB, 115200 );
-
-
+        debugPrintConfigUart(UART_USB, 115200);
 #if (TP1_PROJECT == TP1_2)
         gpioConfig(GPIO0, GPIO_INPUT);
         gpioConfig(GPIO1, GPIO_OUTPUT);
         /* Variable para almacenar el valor de tecla leido */
         bool_t valor;
-
 #elif (TP1_PROJECT == TP1_3)
-        /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
-           periodicamente una interrupcion cada 50ms que incrementa un contador de
-           Ticks obteniendose una base de tiempos). */
-        tickConfig( 50 );
-
-        /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
-           simplemente una funcion que se ejecutara peri­odicamente con cada
-           interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
-           a una interrupcion.
-           El segundo parametro es el parametro que recibe la funcion myTickHook
-           al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+        /*      Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+                periodicamente una interrupcion cada 50ms que incrementa un contador de
+                Ticks obteniendose una base de tiempos). */
+        tickConfig(50);
+        /*      Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+                simplemente una funcion que se ejecutara peri­odicamente con cada
+                interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+                a una interrupcion.
+                El segundo parametro es el parametro que recibe la funcion myTickHook
+                al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
         */
-        tickCallbackSet( myTickHook, (void*)LEDR );
+        tickCallbackSet(myTickHook, (void*)LEDR);
         delay(1000);
-
 #endif
-
-
-        debugPrintString( "DEBUG c/sAPI\r\n" );
-
-
+        debugPrintString("DEBUG c/sAPI\r\n");
         /* ------------- REPETIR POR SIEMPRE ------------- */
         while(1)
         {
@@ -118,23 +95,23 @@ int main(void)
                 valor = !gpioRead(GPIO0);
                 gpioWrite(GPIO1, valor);
 #elif (TP1_PROJECT == TP1_3)
-                tickCallbackSet( myTickHook, (void*)LEDG );
-                debugPrintString( "LED RGB G\n" );
+                tickCallbackSet(myTickHook, (void*)LEDG);
+                debugPrintString("LED RGB G\n");
                 delay(1000);
-                tickCallbackSet( myTickHook, (void*)LEDB );
-                debugPrintString( "LED RGB B\n" );
+                tickCallbackSet(myTickHook, (void*)LEDB);
+                debugPrintString("LED RGB B\n");
                 delay(1000);
-                tickCallbackSet( myTickHook, (void*)LED1 );
-                debugPrintString( "LED 1\n" );
+                tickCallbackSet(myTickHook, (void*)LED1);
+                debugPrintString("LED 1\n");
                 delay(1000);
-                tickCallbackSet( myTickHook, (void*)LED2 );
-                debugPrintString( "LED 2\n" );
+                tickCallbackSet(myTickHook, (void*)LED2);
+                debugPrintString("LED 2\n");
                 delay(1000);
-                tickCallbackSet( myTickHook, (void*)LED3 );
-                debugPrintString( "LED 3\n" );
+                tickCallbackSet(myTickHook, (void*)LED3);
+                debugPrintString("LED 3\n");
                 delay(1000);
-                tickCallbackSet( myTickHook, (void*)LEDR );
-                debugPrintString( "LED RGB R\n" );
+                tickCallbackSet(myTickHook, (void*)LEDR);
+                debugPrintString("LED RGB R\n");
                 delay(1000);
 #endif
         }
@@ -142,5 +119,29 @@ int main(void)
                 por ningun S.O. */
         return 0 ;
 }
+
+
+/*==================[internal functions definition]==========================*/
+
+
+#if (TP1_PROJECT == TP1_3)
+/* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+void myTickHook(void* ptr)
+{
+        static bool_t ledState = OFF;
+        gpioMap_t led = (gpioMap_t)ptr;
+        if(ledState)
+        {
+                ledState = OFF;
+        }
+        else
+        {
+                ledState = ON;
+        }
+        gpioWrite(led, ledState);
+}
+#endif
+
+
 
 /*==================[end of file]============================================*/
